6696ae28445731966907cb2e82cc59d396d8adf5
kevinelong2 <kevinelong@gmail.com>
2019-05-27 10:55:39 -0700

    better stone size math

--- client/client.ts
+++ client/client.ts
@@ -15,11 +15,13 @@ let Action = (action, data) => {
     return {action, ...data};
 };
 
-function onClickPosition(p) {
-    p.innerHTML = 'z'; //In Process "Spinner"
+function onClickBoard(e) {
+    let b = document.getElementById("board");
+    let m = Math.floor(b.offsetWidth / 19);
+
     POST('/game/', Action('add', {
-        'x': this.dataset.x,
-        'y': this.dataset.y
+        'x': Math.floor(e.offsetX / m).toString(),
+        'y': Math.floor(e.offsetY / m).toString()
     })).then(updatePositionSVG)
 }
 
@@ -34,24 +36,26 @@ const CreateSVG = (name) => document.createElementNS(svgNS, name);
 const CircleSVG = () => CreateSVG("circle");
 
 function updatePositionSVG(data) {
+    let b = document.getElementById("board");
+    let m = Math.floor(b.offsetWidth / 19);
+    let f = Math.floor(m / 2);
+
+    let div = document.createElement('div');
+
     let svg = Svg();
-    svg.setAttribute('height', "100%");
-    svg.setAttribute('width', "100%");
+    svg.setAttribute('height', m.toString());
+    svg.setAttribute('width', m.toString());
 
     let fill: string = data.symbol === 'x' ? 'black' : "white";
     svg.appendChild(setANS(CircleSVG(), {
         id: "circle1",
-        cx: 11,
-        cy: 11,
-        r: 11,
+        cx: f,
+        cy: f,
+        r: f,
         fill: fill,
         stroke: "black"
     }));
 
-    let h = Math.ceil(screen.availHeight / 19);
-    let w = Math.ceil(screen.availWidth / 19);
-    let m = h < w ? h : w;
-    let div = document.createElement('div');
     div.setAttribute('class', "stone");
     div.style.left = (data.x * m) + 1 + 'px';
     div.style.top = (data.y * m) + 1 + 'px';
@@ -59,51 +63,17 @@ function updatePositionSVG(data) {
 
 
     div.appendChild(svg);
-    let b = document.getElementById('board');
     b.appendChild(div);
 
     let s = document.getElementById('status');
     s.innerHTML = data.status;
 }
 
-function createPositionSVG(data, o, i) {
-    let id = o === 'x' ? 'black_stone' : 'white_stone';
-
-    let temp = document.getElementById(id);
-    let d: any = temp.cloneNode();
-
-    if (!d) {
-        console.log(`no such id '${id}'`);
-        return;
-    }
-
-    let x = i % data.size;
-    let y = Math.floor(i / data.size);
-
-    d.dataset.x = x.toString();
-    d.dataset.y = y.toString();
-
-    let h = Math.ceil((screen.availHeight / 19) * 0.95);
-    let w = Math.ceil((screen.availWidth / 19) * 0.95);
-    let s = h < w ? h : w;
-
-    d.style.left = (x * s) + 'px';
-    d.style.top = (y * s) + 'px';
-
-    d.style.position = 'absolute';
-    d.style.display = 'block';
-    d.style.border = '0px solid silver';
-
-    d.setAttribute('id', `p_${d.dataset.x}_${d.dataset.y}`);
-
-    d.onclick = onClickPosition;
-    return d;
-}
-
 function drawSVG(data) {
     let b = document.getElementById("board");
-    data.board.map((o, i) => b.appendChild(createPositionSVG(data, o, i)));
+    // data.board.map((o, i) => b.appendChild(createPositionSVG(data, o, i)));
     document.body.appendChild(b);
+    b.onclick = onClickBoard;
 
     let s = document.createElement("div");
     s.setAttribute("id", "status");
